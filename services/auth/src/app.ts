74bf0e52b41bddd6c2098f48b43fd6bc31a17ef4
raj-prasan <rajprasanj@gmail.com>
2026-06-06 22:00:39 +0530

    added cookie-parser

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import authRouter from "./routes/auth.route.js"
-
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(cors({
@@ -9,6 +9,7 @@ app.use(cors({
   credentials: true
 }))
 app.use(express.json({limit: "16Kb"}))
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter)
 export {app}
