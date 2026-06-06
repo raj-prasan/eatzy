d1276844b10ef1071733684c46fb69944f87a7aa
raj-prasan <rajprasanj@gmail.com>
2026-06-06 17:02:39 +0530

    feat(auth): enhance authentication middleware and add user role management

--- services/auth/src/middlewares/auth.middleware.ts
+++ services/auth/src/middlewares/auth.middleware.ts
@@ -1,17 +1,59 @@
-import { Request,Response,NextFunction } from "express";
-import jwt, {JwtPayload} from "jsonwebtoken"
-import { IUser } from "../models/user.model.js";
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { IUser, User } from "../models/user.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 
-export interface AuthnticatedUser extends Request{
-  user?: IUser | null
-
+export interface AuthnticatedUser extends Request {
+  user?: IUser | null;
 }
-export const isAuth = asyncHandler(async (req: AuthnticatedUser, res: Response, next: NextFunction)=>{
-  const token = req.cookies?.accessToken ||req.header("Authorizaton")?.replace("Bearer ", "")
-  if(!token){
-    throw new ApiError(401,"Unauthorized Request")
-  }
-  const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string)
-})
+export const isAuth = asyncHandler(
+  async (req: AuthnticatedUser, res: Response, next: NextFunction) => {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+    ) as JwtPayload;
+    if (!decodedToken) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = decodedToken.user;
+    next();
+  },
+);
+
+const allowedRoles = ["customer", "rider", "seller"] as const;
+
+type Role = (typeof allowedRoles)[number];
+
+export const addUserRole = asyncHandler(
+  async (req: AuthnticatedUser, res: Response, next: NextFunction) => {
+    if (!req.user?._id) {
+      throw new ApiError(401, "UnauthenticatedUser");
+    }
+    const { role } = req.body as { role: Role };
+
+    if (!allowedRoles.includes(role)) {
+      throw new ApiError(400, "Invalid Role");
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, { role }, { new: true });
+
+    if (!user) {
+      throw new ApiError(401, "User not found.");
+    }
+    const token = jwt.sign({ user }, process.env.JWT_SECRET as string, {
+      expiresIn: "15d",
+    });
+    res.status(200).json({
+      message: "Role selected Successfully",
+      user,
+      token
+    });
+  },
+);
--- services/auth/src/routes/auth.route.ts
+++ services/auth/src/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { loginUser } from "../controllers/auth.controller.js";
+import { addUserRole, isAuth } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router()
 
 authRouter.route("/login").post(loginUser)
+authRouter.route("/add/role").put(isAuth, addUserRole)
 
 export default authRouter
